b5140e02ea1aa15fe31da79fd342c1124bcfdbb1
Stacey McKnight <mcknight.stm@gmail.com>
2020-12-21 13:22:53 -0600

    commit tree 9ee8a61bc4ae1508c833810f2e2fb771ec4e02f0

--- lib/customer.rb
+++ lib/customer.rb
@@ -1,3 +1,30 @@
 class Customer
-  
+    attr_reader :name, :age
+    @@all = []
+
+    def initialize(name, age)
+        @name = name
+        @age = age
+        save
+    end
+
+    def self.all
+        @@all
+    end
+
+    def save
+        self.class.all << self
+    end
+
+    def new_meal(waiter, total, tip=0)
+        Meal.new(waiter, self, total, tip)
+    end
+
+    def meals
+        Meal.all.select {|meal| meal.customer == self}
+    end
+
+    def waiters
+        meals.map {|meal| meal.waiter}
+    end
 end
--- lib/meal.rb
+++ lib/meal.rb
@@ -1,3 +1,21 @@
 class Meal
+    attr_accessor :waiter, :customer, :total, :tip
+    @@all = []
+
+    def initialize(waiter, customer, total, tip=0)
+        @waiter = waiter
+        @customer = customer
+        @total = total
+        @tip = tip
+        save
+    end
+
+    def self.all
+        @@all
+    end
+
+    def save
+        self.class.all << self
+    end
 
 end
